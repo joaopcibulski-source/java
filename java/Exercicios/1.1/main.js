6d1ffc06e0603f9db00579db0d3b70a9c5400a28
joaopcibulski-source <joao_p_cibulski@estudante.sesisenai.org.br>
2026-07-07 16:44:14 -0300

    Add files via upload

---
+++ java/Exercicios/1.1/main.js
@@ -0,0 +1,7 @@
+let nome = "Ana";
+nome = "Carlos";
+
+let idade = 25;
+idade = 30;
+
+console.log(nome, idade);
---
+++ java/Exercicios/1.2/main.js
@@ -0,0 +1,7 @@
+let numero = 321
+let decimal = 3.6
+let nome = "joguer"
+let booleano = true
+let undefined;
+
+console.log(numero, decimal, nome, booleano, undefined);
